6d4b6cc7109b19be6afdf8423cbcabede328c3cc
Andreas Schmitz <schmitz@terrestris.de>
2022-01-10 11:14:23 +0100

    Change input features to complex input

--- src/main/java/de/terrestris/geoserver/wps/InvertedPolygon.java
+++ src/main/java/de/terrestris/geoserver/wps/InvertedPolygon.java
@@ -1,6 +1,7 @@
 package de.terrestris.geoserver.wps;
 
 import org.geoserver.wps.gs.GeoServerProcess;
+import org.geoserver.wps.process.RawData;
 import org.geotools.feature.FeatureIterator;
 import org.geotools.geojson.feature.FeatureJSON;
 import org.geotools.geojson.geom.GeometryJSON;
@@ -36,8 +37,8 @@ public class InvertedPolygon implements GeoServerProcess {
   public String execute(
     @DescribeParameter(
       name = "inputFeatures",
-      description = "The resulting GeoJSON geometry"
-    ) String inputFeatures
+      description = "The input features"
+    ) RawData inputFeatures
   ) {
     FeatureJSON reader = new FeatureJSON();
     GeometryJSON writer = new GeometryJSON();
@@ -45,7 +46,7 @@ public class InvertedPolygon implements GeoServerProcess {
       CoordinateReferenceSystem source = CRS.decode("EPSG:4326");
       CoordinateReferenceSystem target = CRS.decode("EPSG:3857");
       MathTransform transform = CRS.findMathTransform(source, target);
-      FeatureIterator<?> collection = reader.readFeatureCollection(inputFeatures).features();
+      FeatureIterator<?> collection = reader.readFeatureCollection(inputFeatures.getInputStream()).features();
       GeometryFactory factory = new GeometryFactory();
       Envelope envelope = new Envelope(-20000000d, 20000000d, -20000000d, 20000000d);
       Geometry polygon = factory.toGeometry(envelope);
